f63642ef9ce9b5aa968a4d1e7609c2aac01f4630
karanikolopoulos <ckaranikolopoulos@gmail.com>
2025-07-19 03:37:16 +0300

    Fix initializers

--- ca_moe/model/initializers/__init__.py
+++ ca_moe/model/initializers/__init__.py
@@ -1,56 +1,81 @@
-import torch.nn as nn
 from logging import getLogger
 
-from .initializers import bypass, lecun_uniform_, lecun_normal_, variance_scaling
+import torch
+import torch.nn as nn
+
+from .initializers import bypass, lecun_normal, lecun_uniform, variance_scaling
 
 logger = getLogger(__name__)
 
-INITIALIZER_REGISTRY: dict[str, callable] = {
-    # constant Fillers
-    "zeros": nn.init.zeros_, # fill with 0
-    "ones": nn.init.ones_, # fill with 1
-    "constant": nn.init.constant_, # fill with a custom scalar
+init_snake: dict[str, callable] = {
+    # constant fillers
+    "zeros": nn.init.zeros_,  # fill with 0
+    "ones": nn.init.ones_,  # fill with 1
+    "constant": nn.init.constant_,  # fill with a custom scalar
     # identity & sparse (structured)
-    "identity": nn.init.eye_, # identity matrix (only for 2D)
-    "dirac": nn.init.dirac_, # dirac delta (preserve features in conv layers)
-    "sparse": nn.init.sparse_, # initialize sparsely (for large fc layers)
+    "identity": nn.init.eye_,  # identity matrix (only for 2D)
+    "dirac": nn.init.dirac_,  # dirac delta (preserve features in conv layers)
+    "sparse": nn.init.sparse_,  # initialize sparsely (for large fc layers)
     # distributions
-    "random_uniform": nn.init.uniform_, # uniform U(a, b)
-    "random_normal": nn.init.normal_, # normal N(μ, σ)
-    "truncated_normal": nn.init.trunc_normal_, # limited range
+    "random_uniform": nn.init.uniform_,  # uniform U(a, b)
+    "random_normal": nn.init.normal_,  # normal N(μ, σ)
+    "truncated_normal": nn.init.trunc_normal_,  # limited range
     # Glorot (Xavier) - based on fan_avg (good for sigmoid, tanh)
     "glorot_uniform": nn.init.xavier_uniform_,
     "glorot_normal": nn.init.xavier_normal_,
     # He (Kaiming) - based on fan_in (good for ReLU)
     "he_uniform": nn.init.kaiming_uniform_,
     "he_normal": nn.init.kaiming_normal_,
-    # Lecun - based on fan_in (good for SELU or scaled tanh) 
-    "lecun_uniform": lecun_uniform_,
-    "lecun_normal": lecun_normal_,
     # Orthogonal - maintains orthogonality in linear ops
     "orthogonal": nn.init.orthogonal_,
-    # scaled - generalized, supports all modes + distributions 
+    # Lecun - based on fan_in (good for SELU or scaled tanh)
+    "lecun_uniform": lecun_uniform,
+    "lecun_normal": lecun_normal,
+    # scaled - generalized, supports all modes + distributions
     "variance_scaling": variance_scaling,
-    # debug - passthrough fn
-    "bypass": bypass 
 }
+
+
+init_pascal: dict[str, callable] = {
+    "Zeros": nn.init.zeros_,
+    "Ones": nn.init.ones_,
+    "Constant": nn.init.constant_,
+    "Identity": nn.init.eye_,
+    "Dirac": nn.init.dirac_,
+    "Sparse": nn.init.sparse_,
+    "RandomUniform": nn.init.uniform_,
+    "RandomNormal": nn.init.normal_,
+    "TruncatedNormal": nn.init.trunc_normal_,
+    "GlorotUniform": nn.init.xavier_uniform_,
+    "GlorotNormal": nn.init.xavier_normal_,
+    "HeUniform": nn.init.kaiming_uniform_,
+    "HeNormal": nn.init.kaiming_normal_,
+    "Orthogonal": nn.init.orthogonal_,
+    "LecunUniform": lecun_uniform,
+    "LecunNormal": lecun_normal,
+    "VarianceScaling": variance_scaling,
+}
+
+INITIALIZER_REGISTRY = init_snake | init_pascal | {"bypass": bypass}
+
 
 def get_initializer(name: str | None, **kwargs) -> callable:
     if name is None:
         logger.info("No initializer name provided.")
-        return bypass
-    
+        initializer_fn = bypass
+
     try:
         initializer_fn = INITIALIZER_REGISTRY[name]
-        logger.info(f"Using regulizer: {name}")
-        return initializer_fn(**kwargs)
+        logger.info(f"Using initializer: {name}")
     except KeyError as e:
         options = ", ".join(INITIALIZER_REGISTRY.keys())
-        
+
         logger.info(repr(e))
         logger.info(f"Unknown initializer: {name}. Available options: {options}")
         logger.info("Fallback to no initializer.")
-        return bypass
+        initializer_fn = bypass
 
+    def wrap(tensor: torch.Tensor) -> torch.Tensor:
+        initializer_fn(tensor=tensor, **kwargs)
 
-
+    return wrap
--- ca_moe/model/initializers/initializers.py
+++ ca_moe/model/initializers/initializers.py
@@ -1,26 +1,69 @@
 import torch
+import math
+from torch.nn.init import _calculate_fan_in_and_fan_out, trunc_normal_, normal_, uniform_
 
+TRUN_NORMAL_STDDEV_CORRECTION = 0.87962566103423978
+
+def validate_inputs(scale: float, mode: str, distribution: str):
+    if scale <= 0.0:
+        raise ValueError(
+                "Argument `scale` must be positive float. "
+                f"Received: scale={scale}"
+            )
+    
+    allowed_modes = {"fan_in", "fan_out", "fan_avg"}
+    if mode not in allowed_modes:
+        raise ValueError(
+            f"Invalid `mode` argument: {mode}. "
+            f"Please use one of {allowed_modes}"
+        )
+
+    allowed_distributions = {
+            "uniform",
+            "truncated_normal",
+            "untruncated_normal",
+        }
+    if distribution not in allowed_distributions:
+        raise ValueError(
+            f"Invalid `distribution` argument: {distribution}."
+            f"Please use one of {allowed_distributions}"
+        )
+
+def variance_scaling(
+    tensor: torch.Tensor,
+    scale: float = 1.0,
+    mode: str = "fan_in",
+    distribution: str = "truncated_normal"
+) -> torch.Tensor:
+    
+    validate_inputs(scale=scale, mode=mode, distribution=distribution)
+
+    fan_in, fan_out = _calculate_fan_in_and_fan_out(tensor)
+    denom = {
+        "fan_in": fan_in,
+        "fan_out": fan_out,
+        "fan_avg": (fan_in + fan_out) / 2,
+    }[mode]
+    denom = max(1.0, denom)
+    variance = scale / denom
+
+    if distribution == "truncated_normal":
+        stddev = math.sqrt(variance) / TRUN_NORMAL_STDDEV_CORRECTION
+        return trunc_normal_(tensor, mean=0.0, std=stddev, a=-2*stddev, b=2*stddev)
+
+    elif distribution == "untruncated_normal":
+        stddev = math.sqrt(variance)
+        return normal_(tensor, mean=0.0, std=stddev)
+
+    elif distribution == "uniform":
+        limit = math.sqrt(3.0 * variance)
+        return uniform_(tensor, a=-limit, b=limit)
+
+def lecun_uniform(tensor: torch.Tensor) -> torch.Tensor:
+    return variance_scaling(tensor, scale=1.0, mode="fan_in", distribution="uniform")
+
+def lecun_normal(tensor: torch.Tensor) -> torch.Tensor:
+    return variance_scaling(tensor, scale=1.0, mode="fan_in", distribution="truncated_normal")
 
 def bypass(tensor: torch.Tensor) -> torch.Tensor:
     return tensor
-
-
-def _lecun(tensor: torch.Tensor) -> tuple[int, torch.Tensor]:
-    fan_in = torch.nn.init._calculate_correct_fan(tensor, mode="fan_in")
-    std = torch.sqrt(torch.tensor(1.0 / fan_in, dtype=tensor.dtype, device=tensor.device))
-    return fan_in, std
-
-
-def lecun_normal_(tensor: torch.Tensor) -> torch.Tensor:
-    _, std = _lecun(tensor)
-    return torch.nn.init.normal_(tensor, mean=0.0, std=std)
-
-
-def lecun_uniform_(tensor: torch.Tensor) -> torch.Tensor:
-    _, std = _lecun(tensor)
-    bound = torch.sqrt(torch.tensor(3.0, dtype=tensor.dtype, device=tensor.device)) * std
-    return torch.nn.init.uniform_(tensor, a=-bound.item(), b=bound.item())
-
-
-def variance_scaling(scale=1.0, mode="fan_in", distribution="truncated_normal"):
-    raise NotImplementedError
